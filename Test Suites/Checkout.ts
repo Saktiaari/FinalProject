c921a07a399bdc5365de7b6c4efa0ed35352135a
rizky <rizky@Rizky>
2024-02-12 11:01:06 +0700

    Update Test Suite

--- Test Suites/Checkout.ts
+++ Test Suites/Checkout.ts
@@ -5,10 +5,17 @@
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
-   <numberOfRerun>0</numberOfRerun>
+   <numberOfRerun>3</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
-   <rerunImmediately>false</rerunImmediately>
+   <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>de97713c-81af-4b89-b863-b575a383a6b1</testSuiteGuid>
+   <testCaseLink>
+      <guid>326bd108-1050-4908-9d1f-d7e4701d21f2</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Checkout</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
 </TestSuiteEntity>
